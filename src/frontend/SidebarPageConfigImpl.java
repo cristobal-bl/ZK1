5b22a7bf1255b55cd1552bcd2e5a4b32d32e711a
Cristobal <cbl3685@gmail.com>
2019-04-22 17:58:36 -0600

    Added page-based navigation (C6.1)

--- src/frontend/SidebarPageConfigImpl.java
+++ src/frontend/SidebarPageConfigImpl.java
@@ -10,8 +10,10 @@ public class SidebarPageConfigImpl implements SidebarPageConfig{
 	HashMap<String,SidebarPage> pageMap = new LinkedHashMap<String,SidebarPage>();
 	public SidebarPageConfigImpl() {
 		pageMap.put("fn1",new SidebarPage("zk", "ZK", "/FEFiles/img/image.png", "http://zkoss.org"));
-		pageMap.put("fn2",new SidebarPage("demo", "ZK Demo", "image.png", "http://zkoss.org"));
-		pageMap.put("fn3",new SidebarPage("devref", "ZK Developer Reference", "image.png", "http://zkoss.org"));
+		pageMap.put("fn2",new SidebarPage("demo", "ZK Demo", "/FEFiles/img/image.png", "http://zkoss.org"));
+		pageMap.put("fn3",new SidebarPage("devref", "ZK Developer Reference", "/FEFiles/img/image.png", "http://zkoss.org"));
+		pageMap.put("fn4",new SidebarPage("fn1","Profile (MVC)","/FEFiles/img/image.png","/FEFiles/ZUL/index-profile-mvc.zul"));
+		pageMap.put("fn5",new SidebarPage("fn3","Todo List (MVC)","/FEFiles/img/image.png","/FEFiles/ZUL/index-todolist-mvc.zul"));
 	}
 	
 	//Below: Mandatory code because of interface implementation
